e15eae7efc34d3eb54458f592b27a92b70fe21da
EC2 Default User <sukanya.sravasti22@gmail.com>
2020-09-08 10:20:43 +0000

    message

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -50,22 +50,58 @@ end
 #print sum_to_n?([1,1,2,3,1],5)
 
 
-# # # Part 2
+# # Part 2
 
-# # def hello(name)
-# #   # YOUR CODE HERE
-# # end
+def hello(name)
+  #print "Hello #{name}"
+  return "Hello, #{name}"
+end
 
-# # def starts_with_consonant? s
-# #   # YOUR CODE HERE
-# # end
+#print hello("Dan")
 
-# # def binary_multiple_of_4? s
-# #   # YOUR CODE HERE
-# # end
+def starts_with_consonant?(s)
+#i at the end to make regular expression case insensitive.
+#\W anything that’s not in [0-9a-zA-Z_]
+#[^aeiou] means a range of character except a e i o u [^ inside is negated range]
+#s[0] gives first character
+#numbers behave like r,s etc (consonant), # is example of non-word character
+  if /[^aeiou\W]/i.match(s[0]) == nil
+    return false
+  else
+    return true
+  end  
+end
 
-# # # Part 3
+print (starts_with_consonant?("9hea"))
 
-# # class BookInStock
-# # # YOUR CODE HERE
-#end
+def binary_multiple_of_4?(s)
+  if (s) == "0"
+    return true
+  end
+  #binary mutliples of 4 will look like 010101010000 (i.e. end with at least two zeros)
+  # ^ denotes begining, * denotes [01](i.e. last string) repeats and (00)$ means string ends with (00)
+  if /^[01]*(00)$/.match(s) 
+    return true
+  else return false
+  end
+end
+
+
+# # Part 3
+class BookInStock
+  #what properties the instances of this class will have
+  attr_accessor :isbn, :price
+
+  #constructor
+  def initialize isbn, price
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format("$%.2f", @price)
+  end
+
+end
+
